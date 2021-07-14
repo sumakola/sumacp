7a1684d48637eec361cdec31923745eec9123965
sumakola <sumakola1106@msitprogram.net>
2021-07-14 19:33:50 +0530

    power sum

--- powersum.py
+++ powersum.py
@@ -10,10 +10,25 @@
 # If n is negative, return 0. Similarly, if k is negative, 
 # return 0.
 
-def powerSum(n, k):
-    # Your code goes here...
-    return 0
+# powerSum(n, k) [15 pts]
+# Write the function powerSum(n, k) that takes two 
+# possibly-negative integers n and k and returns the 
+# so-called power sum: Specify the time complexity based 
+# on the solution that you have given. [Hint: This can be 
+# solved in (n * log k)  or better.
 
+#    1**k + 2**k + ... + n**k
+
+# If n is negative, return 0. Similarly, if k is negative, 
+# return 0.
+
+def powerSum(n, k):
+    sum = 0
+    for i in range(1, n + 1, 1):
+         
+        sum += pow(i, k)
+
+    return sum
 # Write your own test cases here...
 
-print ("All test cases passed...")
+#print ("All test cases passed...")
