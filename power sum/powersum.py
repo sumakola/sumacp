44d92cb40f51240686b46f26fbaf5fd968f4338b
Suma Kola <sumakola1106@msitprogram.net>
2021-07-15 00:57:30 +0530

    Update powersum.py

--- power sum/powersum.py
+++ power sum/powersum.py
@@ -1,14 +1,3 @@
-# powerSum(n, k) [15 pts]
-# Write the function powerSum(n, k) that takes two 
-# possibly-negative integers n and k and returns the 
-# so-called power sum: Specify the time complexity based 
-# on the solution that you have given. [Hint: This can be 
-# solved in (n * log k)  or better.
-
-#    1**k + 2**k + ... + n**k
-
-# If n is negative, return 0. Similarly, if k is negative, 
-# return 0.
 
 # powerSum(n, k) [15 pts]
 # Write the function powerSum(n, k) that takes two 
