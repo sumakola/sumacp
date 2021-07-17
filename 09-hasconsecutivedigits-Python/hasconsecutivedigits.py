d10252959043511fe29b4a8a4b4ad7988f7c08af
sumakola <sumakola1106@msitprogram.net>
2021-07-17 22:34:59 +0530

    consecutive digits soln

--- 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
+++ 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
@@ -3,5 +3,16 @@
 # number contains two consecutive digits that are the same, and False otherwise.
 
 def hasconsecutivedigits(n):
-	# your code goes here
-	pass
+	x= abs(n)
+	if x==0:
+		return False
+	else:
+		while(x>0):
+			
+			y=x%10
+			z=y
+			x=int(x//10)
+			y=x%10
+			if( z== y):
+				return True
+		return False        
