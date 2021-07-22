f0cb2542d603ee50fcdba2032418d4b71119be18
sumakola <sumakola1106@msitprogram.net>
2021-07-22 10:56:07 +0530

    vowe

---
+++ vowelCount_python/vowelcount.py
@@ -0,0 +1,8 @@
+def vowelcount(n):
+    count=0
+    for i in n:
+        if i == 'A' or i=='E' or i== 'I' or i=='O' or i=='U' or i=='a' or i=='e' or i=='i' or i=='o' or i=='u':
+            count+=1
+    return count
+
+print(vowelcount("Suma"))
