5550514c61c62b66acd5701c8adb882af5692495
sumakola <sumakola1106@msitprogram.net>
2021-07-26 12:50:25 +0530

    rotated soln

--- 08-isrotated-Python/isrotated.py
+++ 08-isrotated-Python/isrotated.py
@@ -1,9 +1,17 @@
 # isRotated(str1, str2) [10 pts]
-# Write an efficient program to test if two given String is a rotation of each other or not, e.g. 
-# if given String is "XYZ" and "ZXY" then your function should return true, but if the input is 
+# Write an efficient program to test if two given String is a rotation of each other or not, e.g.
+# if given String is "XYZ" and "ZXY" then your function should return true, but if the input is
 # "XYZ" and "YXZ" then return false.
+
+def rotateRight(s):
+    return s[1:len(s)+1] + s[0]
 
 
 def isrotated(str1, str2):
-	#Your code goes here
-	pass
+    # Your code goes here
+    for i in range(len(str1)):
+        if(rotateRight(str1) == str2):
+            return True
+        else:
+            str1 = rotateRight(str1)
+    return False
