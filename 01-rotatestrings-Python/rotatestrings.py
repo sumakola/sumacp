ade7687acafc56945d0d1ed5c39bd8f84e68f044
sumakola <sumakola1106@msitprogram.net>
2021-07-27 09:59:50 +0530

    rotate strings -left and right soln

--- 01-rotatestrings-Python/rotatestrings.py
+++ 01-rotatestrings-Python/rotatestrings.py
@@ -1,11 +1,31 @@
-# Write the function rotatestring(s, k) that takes a string s and a possibly-negative integer k. 
-# If k is non-negative, the function returns the string s rotated k places to the left. 
+# Write the function rotatestring(s, k) that takes a string s and a possibly-negative integer k.
+# If k is non-negative, the function returns the string s rotated k places to the left.
 # If k is negative, the function returns the string s rotated |k| places to the right. So, for example:
 # assert(rotateString('abcd',  1) == 'bcda')
 # assert(rotateString('abcd', -1) == 'dabc')
 
 
+def rotateStringLeft(s, n):
+    k = len(s)
+    n = n % k
+    x = s[n:k]
+    y = s[0: n]
+    return (x + y)
+
+
+def rotateStringRight(s, n):
+    k = len(s)
+    n1 = abs(n)
+    n1 = n1 % k
+    x = s[k-n1:k]
+    y = s[0:k-n1]
+    return (x+y)
+
 
 def fun_rotatestrings(s, n):
-	return s
-
+    if(n > 0):
+        return(rotateStringLeft(s, n))
+    elif(n == 0):
+        return(s)
+    else:
+        return(rotateStringRight(s, n))
