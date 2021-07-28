394705aefe42d10407e5b95495d08d524b13c196
sumakola <sumakola1106@msitprogram.net>
2021-07-28 16:20:10 +0530

    is sorted

--- 10-issorted-Python/issorted.py
+++ 10-issorted-Python/issorted.py
@@ -5,39 +5,11 @@
 # learn soon, it runs in O(n) time, where n=len(a)), and so in particular you may not sort
 # the list.
 
-# to check this is run a loop for first element and check if we could find any smaller element than it after that element, if yes, the list is not sorted
-def isAscending(a):
-    flag = 0
-    a1 = a[:]
-    a1.sort()
-    if (a1 == a):
-        flag = 1
-
-# printing result
-    if (flag):
-        return True
-    else:
-        return False
-
-# run a loop for first element and check if we could find any larger element than it after that element
-
-
-def isDescending(a):
-    flag = 0
-    i = 1
-    while i < len(a):
-        if(a[i] > a[i - 1]):
-            flag = 1
-        i += 1
-
-# printing result
-    if (not flag):
-        return True
-    else:
-        return False
-
-
 def issorted(a):
-    if(isAscending(a) or (isDescending(a))):
+    # your code goes here
+    if(a == sorted(a)):
         return True
-    return False
+    if (a == sorted(a, reverse=True)):
+        return True
+    else:
+        return False
