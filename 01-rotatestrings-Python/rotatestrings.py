8d0f6fb31b933c2506491ffcb68a75bec35ff2d4
sumakola <sumakola1106@msitprogram.net>
2021-07-28 16:22:44 +0530

    rotate strings

--- 01-rotatestrings-Python/rotatestrings.py
+++ 01-rotatestrings-Python/rotatestrings.py
@@ -5,27 +5,7 @@
 # assert(rotateString('abcd', -1) == 'dabc')
 
 
-def rotateStringLeft(s, n):
-    k = len(s)
-    n = n % k
-    x = s[n:k]
-    y = s[0: n]
-    return (x + y)
-
-
-def rotateStringRight(s, n):
-    k = len(s)
-    n1 = abs(n)
-    n1 = n1 % k
-    x = s[k-n1:k]
-    y = s[0:k-n1]
-    return (x+y)
-
-
 def fun_rotatestrings(s, n):
-    if(n > 0):
-        return(rotateStringLeft(s, n))
-    elif(n == 0):
-        return(s)
-    else:
-        return(rotateStringRight(s, n))
+    n = n % len(s)
+    rotate = s[n:] + s[:n]
+    return rotate
