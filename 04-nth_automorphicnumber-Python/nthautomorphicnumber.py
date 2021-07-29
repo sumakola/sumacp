7b387e1232ecade7da8bb308c6dd13a636eedcfa
sumakola <sumakola1106@msitprogram.net>
2021-07-29 11:52:56 +0530

    nth automorphic soln

--- 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
+++ 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
@@ -1,8 +1,26 @@
 # nthAutomorphicNumbers(n) [20 pts]
-# In mathematics, an automorphic number is a number whose square "ends" in the same digits as the 
-# number itself. For example, 5^2 = 25, 6^2 = 36, 76^2 = 5776, and 890625^2 = 793212890625, so 5, 6, 
+# In mathematics, an automorphic number is a number whose square "ends" in the same digits as the
+# number itself. For example, 5^2 = 25, 6^2 = 36, 76^2 = 5776, and 890625^2 = 793212890625, so 5, 6,
 # 76 and 890625 are all automorphic numbers.
 
+def isAutomorphicNum(n):
+
+    Num = n
+    square = Num * Num
+    while (Num > 0):
+        if(Num % 10 != square % 10):
+            return False
+        Num = int(Num//10)
+        square = int(square//10)
+    return True
+
+
 def nthautomorphicnumbers(n):
-	# Your code goes here
-	pass
+    # Your code goes here
+    f = 2
+    g = 0
+    while(f <= n):
+        g += 1
+        if(isAutomorphicNum(g)):
+            f += 1
+    return g
