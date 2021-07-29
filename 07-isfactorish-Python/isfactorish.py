d739a4c39864389ffa5c8ef4efa5f79f5dddbfe0
sumakola <sumakola1106@msitprogram.net>
2021-07-29 17:47:29 +0530

    is factorish soln

--- 07-isfactorish-Python/isfactorish.py
+++ 07-isfactorish-Python/isfactorish.py
@@ -1,6 +1,6 @@
-# Write the function fun_isfactorish(n) that takes a value int n, 
-# and returns True if n is a (possibly-negative) integer with exactly 3 unique digits 
-# (so no two digits are the same), where each of the digits is a factor of the number 
+# Write the function fun_isfactorish(n) that takes a value int n,
+# and returns True if n is a (possibly-negative) integer with exactly 3 unique digits
+# (so no two digits are the same), where each of the digits is a factor of the number
 # n itself. In all other cases, the function returns False (without crashing).
 # For example:
 #  assert(fun_isfactorish(412) == True) # 4, 1, and 2 are all factors of 412
@@ -11,6 +11,32 @@
 #  assert(fun_isfactorish(42) == False) # 42 has a leading 0 (only 2 unique digits)
 
 
-def fun_isfactorish(n):
-	return False
+def digitCount(n):
+    count = 0
+    while(n > 0):
+        n = n//10
+        count += 1
+    return count
 
+
+def fun_isfactorish(n):
+    t = n
+    if (n < 0):
+        n = -n
+
+    if(digitCount(n) != 3):
+        return False
+    digitOne = n % 10
+    n = n // 10
+    digitTwo = n % 10
+    n = n // 10
+    digitThree = n % 10
+
+    if(digitOne == 0 or digitTwo == 0 or digitThree == 0):
+        return False
+    elif(digitOne == digitTwo or digitTwo == digitThree or digitThree == digitOne):
+        return False
+    elif (t % digitOne == 0 and t % digitTwo == 0 or t % digitThree == 0):
+        return True
+    else:
+        return False
