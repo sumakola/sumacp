ecceab2ed98473790977b4a3e9637b576f52a4ab
sumakola <sumakola1106@msitprogram.net>
2021-07-30 12:05:14 +0530

    even digitss only.

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -9,6 +9,24 @@
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
 
+def checkEvenDigits(number, newNum=0, powers=1):
+    digit = number % 10
+    if number <= 0:
+        return newNum
+    else:
+
+        if digit % 2 == 0:
+
+            newNum += digit * powers
+            number //= 10
+
+            powers *= 10
+            return checkEvenDigits(number, newNum, powers)
+        else:
+            number //= 10
+            return checkEvenDigits(number, newNum, powers)
+
+
 def fun_recursion_onlyevendigits(l, finl=None, count=0):
     if finl == None:
         finl = []
@@ -18,27 +36,7 @@ def fun_recursion_onlyevendigits(l, finl=None, count=0):
         return finl
     else:
         count += 1
-        # we will call our helper fuction on every number to convert them into
-        # numbers made of only even numbers
+
         newNum = checkEvenDigits(l[0])
         finl.append(newNum)
         return fun_recursion_onlyevendigits(l[1:], finl, count)
-
-
-def checkEvenDigits(number, newNum=0, powers=1):
-    # base case checks if we have already loooped through the whole number
-    digit = number % 10
-    if number <= 0:
-        return newNum
-    else:
-        # first we get the number from the very left
-        if digit % 2 == 0:
-            # add the firstDigit to our newNum
-            newNum += digit * powers
-            number //= 10
-            # strip down the number by 10
-            powers *= 10
-            return checkEvenDigits(number, newNum, powers)
-        else:
-            number //= 10
-            return checkEvenDigits(number, newNum, powers)
