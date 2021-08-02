2f592c91de66dea87515bade84681d4aba939492
sumakola <sumakola1106@msitprogram.net>
2021-08-02 08:34:39 +0530

    non destructive remove repeats soln

--- non_destructive_remove_repeats.py
+++ non_destructive_remove_repeats.py
@@ -10,9 +10,17 @@
 
 def nondestructiveRemoveRepeats(L):
     # Your code goes here...
-    pass
+    result = []
+    for ele in L:
+        if ele not in result:
+            result.append(ele)
+    return result
 
-assert(nondestructiveRemoveRepeats([1, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 3, 5, 2, 7])
-assert(nondestructiveRemoveRepeats([1, 5, 3, 3, 2, 1, 7, 5]) == [1, 5, 3, 2, 7])
-assert(nondestructiveRemoveRepeats([1, 2, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 2, 3, 5, 7])
-print ("All test cases passed....")
+
+assert(nondestructiveRemoveRepeats(
+    [1, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 3, 5, 2, 7])
+assert(nondestructiveRemoveRepeats(
+    [1, 5, 3, 3, 2, 1, 7, 5]) == [1, 5, 3, 2, 7])
+assert(nondestructiveRemoveRepeats(
+    [1, 2, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 2, 3, 5, 7])
+print("All test cases passed....")
