db85b1afa4457affed73e4e264b6a77a0c07d2ce
sumakola <sumakola1106@msitprogram.net>
2021-08-03 23:50:32 +0530

    is rectangle soln

--- 01-isrectangular-Python/isrectangular.py
+++ 01-isrectangular-Python/isrectangular.py
@@ -1,11 +1,12 @@
-# Write the function isrectangular(L) that takes a possibly-2d 
+# Write the function isrectangular(L) that takes a possibly-2d
 # list L and returns True  if it is rectangular, so each row has
 #  the same number of elements. Return False otherwise.
 
 
 def fun_isrectangular(l):
-	# Your code goes here...
-	return False
-
-
-
+    # Your code goes here...
+    n = l
+    for i in n:
+        if len(i) != len(n[0]):
+            return False
+    return True
