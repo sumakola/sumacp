0217c65d88ccc63fbe662f9da19b77cf4ed62319
sumakola <sumakola1106@msitprogram.net>
2021-08-04 11:23:02 +0530

    second largest soln

--- 06-recursion_secondlargest-Python/recursion_secondlargest.py
+++ 06-recursion_secondlargest-Python/recursion_secondlargest.py
@@ -1,8 +1,8 @@
 # Recursion-Only recursion_secondlargest(L) [15 pts]
-# Note: recall that you may not use sort, sorted, min, or max this week! With that in mind, write the function 
-# recursion_secondlargest(L) that takes a list of integers in any order and returns the second-largest value in the list. To 
-# be more precise, it returns the second value from the end if the list was sorted (though you do not need to sort 
-# it to solve this problem), so if the largest value occurs twice, you would return that value. Also, if there are 
+# Note: recall that you may not use sort, sorted, min, or max this week! With that in mind, write the function
+# recursion_secondlargest(L) that takes a list of integers in any order and returns the second-largest value in the list. To
+# be more precise, it returns the second value from the end if the list was sorted (though you do not need to sort
+# it to solve this problem), so if the largest value occurs twice, you would return that value. Also, if there are
 # fewer than 2 values in the list, return None. Here are some test cases:
 # assert(recursion_secondlargest([1,2,3,4,5]) == 4)
 # assert(recursion_secondlargest([4,3]) == 3)
@@ -10,9 +10,26 @@
 # assert(recursion_secondlargest([-3,-4]) == -4)
 # assert(recursion_secondlargest([4]) == None)
 # assert(recursion_secondlargest([ ]) == None)
-# Again, you do not need to sort the list. We didn't sort it in our sample solution. We just tracked the two largest 
+# Again, you do not need to sort the list. We didn't sort it in our sample solution. We just tracked the two largest
 # values as we recursively traversed the list. Also, you may not use loops/iteration in this problem
 
 def recursion_secondlargest(L):
-	# Your code goes here
-	pass
+    # Your code goes here
+    k = 2
+    z = len(L)
+    if(z <= 1):
+        return None
+    else:
+        return kthmax(k, L)
+
+
+def kthmax(k, list):
+    f = max(list)
+    lstCount = list.count(f)
+    if(lstCount >= 2):
+        return f
+    if (k == 1):
+        return max(list)
+    else:
+        m = max(list)
+        return(kthmax(k-1, [x for x in list if x != m]))
