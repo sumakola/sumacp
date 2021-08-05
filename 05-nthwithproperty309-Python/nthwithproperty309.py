88ef2e0daa4971d21323c7581b418134a3804df8
sumakola <sumakola1106@msitprogram.net>
2021-08-05 15:55:41 +0530

    nth with property 309 soln

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -1,9 +1,25 @@
 # nthWithProperty309(n) [20 pts]
-# We will say that a number n has "Property309" if its 5th power contains every 
-# digit (from 0 to 9) at least once. 309 is the smallest number with this property. 
-# Write the function nthWithProperty309 that takes a non-negative int n and returns 
+# We will say that a number n has "Property309" if its 5th power contains every
+# digit (from 0 to 9) at least once. 309 is the smallest number with this property.
+# Write the function nthWithProperty309 that takes a non-negative int n and returns
 # the nth number with Property309.
 
+import re
+
+
+def prop309(n):
+    n = (n**5)
+    if(len(set("".join(re.findall(r'([\d])', str(n))))) == 10):
+        return True
+    return False
+
+
 def nthwithproperty309(n):
-	# Your code goes here
-	pass
+    # Your code goes here
+    x = 0
+    y = 0
+    while(x <= n):
+        y += 1
+        if(prop309(y)):
+            x += 1
+    return y
