746618909aebab876b99005db58e58357c55ce67
sumakola <sumakola1106@msitprogram.net>
2021-08-06 11:54:57 +0530

    nth happy number soln

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -1,5 +1,5 @@
-# Write the function nth_happy_number(n) which takes a non-negative integer 
-# and returns the nth happy number (where the 0th happy number is 1). 
+# Write the function nth_happy_number(n) which takes a non-negative integer
+# and returns the nth happy number (where the 0th happy number is 1).
 # Here are some test assertions for you:
 
 # https://en.wikipedia.org/wiki/Happy_number#:~:text=In%20number%20theory%2C%20a%20happy,starting%20with%20and%20eventually%20reaches
@@ -13,35 +13,33 @@
 # assert(nth_happy_number(6) == 23)
 # assert(nth_happy_number(7) == 28)
 # assert(nth_happy_number(8) == 31)
-#lognbase10
-def findDigitSquaresum(n, sum = 0):
-	if(n>0):
-		sum+=(n%10)**2
-		n=n//10
-		return findDigitSquaresum(n, sum)
-	return sum
+# lognbase10
 
-# print (findDigitSquaresum(125))
-# assert(findDigitSquaresum(125) == 30)
-# assert(findDigitSquaresum(1) == 1)
-# assert(findDigitSquaresum(0) == 0)
-# assert(findDigitSquaresum(12) == 5)
-# print ("All test cases passed. :-)")
+# reference from https://www.geeksforgeeks.org/happy-numbers/
+
+def findDigitSquaresum(n, sum=0):
+    if(n > 0):
+        sum += (n % 10)**2
+        n = n//10
+        return findDigitSquaresum(n, sum)
+    return sum
+
 
 def ishappynumber(n):
-	sum = findDigitSquaresum(n)
-	if sum == 1:
-			return True
-	elif sum<10:
-		return False
-	else:
-		return ishappynumber(sum)
-def nth_happy_number(n):
-	x = 1
-	y = 0
-	while(x<=abs(n)):
-		y+=1
-		if(ishappynumber(y)):
-			x+=1
-	return y
+    sum = findDigitSquaresum(n)
+    if sum == 1:
+        return True
+    elif sum < 10:
+        return False
+    else:
+        return ishappynumber(sum)
 
+
+def nth_happy_number(n):
+    x = 1
+    y = 0
+    while(x <= abs(n)):
+        y += 1
+        if(ishappynumber(y)):
+            x += 1
+    return y
