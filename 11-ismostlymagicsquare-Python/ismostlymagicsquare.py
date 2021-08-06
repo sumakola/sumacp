77eb1d3c98e57db2241626ac6a5810d8ff44838a
sumakola <sumakola1106@msitprogram.net>
2021-08-06 11:41:46 +0530

    is mostly magic square soln

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -1,9 +1,9 @@
 # isMostlyMagicSquare(a) [15 pts]
-# Write the function isMostlyMagicSquare(a) that takes an 2d list of numbers, which you may assume is an NxN square 
+# Write the function isMostlyMagicSquare(a) that takes an 2d list of numbers, which you may assume is an NxN square
 # with N>0, and returns True if it is "mostly magic" and False otherwise, where a square is "mostly magic" if:
 # Each row, each column, and each of the 2 diagonals each sum to the same total.
-# A completely magic square has additional restrictions (such as not allowing duplicates, and only allowing numbers 
-# from 1 to N2), which we are not enforcing here, but which you can read about here. Note: any magic square is also 
+# A completely magic square has additional restrictions (such as not allowing duplicates, and only allowing numbers
+# from 1 to N2), which we are not enforcing here, but which you can read about here. Note: any magic square is also
 # a "mostly magic" square, including this sample magic square:
 # Read for more: https://en.wikipedia.org/wiki/Magic_square
 # Here is another mostly-magic square:
@@ -13,21 +13,21 @@
 #   [ 2, 1]]
 # Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
 
+# reference from https://www.youtube.com/watch?v=jbwPIOCV39g
 def ismostlymagicsquare(a):
-	for i in range(len(a)):
-		sumRows=0
-		sumCols=0
-		for j in range(len(a[0])):
-			sumRows+=a[i][j]
-			sumCols+=a[j][i]
-		if sumCols!=sumRows:
-			return False
-	diagOne=0
-	diagTwo=0
-	for i in range(len(a)):
-		diagOne+=a[i][i]
-		diagTwo+=a[i][len(a)-i-1] 
-	if(diagOne!=diagTwo):
-		return False
-	return True
-
+    for i in range(len(a)):
+        sumRows = 0
+        sumCols = 0
+        for j in range(len(a[0])):
+            sumRows += a[i][j]
+            sumCols += a[j][i]
+        if sumCols != sumRows:
+            return False
+    diagOne = 0
+    diagTwo = 0
+    for i in range(len(a)):
+        diagOne += a[i][i]
+        diagTwo += a[i][len(a)-i-1]
+    if(diagOne != diagTwo):
+        return False
+    return True
