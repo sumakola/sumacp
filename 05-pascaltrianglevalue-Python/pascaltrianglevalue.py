ce9e3ff8a2b6bb7e94c155a49b7d63bcdefe7fcd
sumakola <sumakola1106@msitprogram.net>
2021-08-06 11:08:48 +0530

    PASCAL TRIANGLE VALUE

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -1,30 +1,32 @@
-# Write the function pascalsTriangleValue(row, col) 
-# that takes int values row and col, and returns the 
-# value in the given row and column of Pascal's 
-# Triangle where the triangle starts at row 0, and 
-# each row starts at column 0. If either row or col 
-# are not legal values, return None, instead of crashing. 
+# Write the function pascalsTriangleValue(row, col)
+# that takes int values row and col, and returns the
+# value in the given row and column of Pascal's
+# Triangle where the triangle starts at row 0, and
+# each row starts at column 0. If either row or col
+# are not legal values, return None, instead of crashing.
 
 
+# REFERENCE FROM https://www.youtube.com/watch?v=EJLvbgJvlBk
 
-
-#to find the factorial of number 
+# to find the factorial of number
 #
 def factorial(n):
-	if n == 0:
-		return 1
-	return (n * factorial(n-1))
+    if n == 0:
+        return 1
+    return (n * factorial(n-1))
 
-#to find the value in the given row and column
-#using ncr formula as each value can be given as a combination of rows and columns
+# to find the value in the given row and column
+# using ncr formula as each value can be given as a combination of rows and columns
+
+
 def fun_pascaltrianglevalue(row, col):
-	if(row==col and col==1):
-		return 1
-	#as ncn-r and ncr will have same value and will be equal to n
-	elif(col==row-1 and col==1):
-		return(row)
-	#ncr formula is not applicable when n<r
-	elif(col>row):
-		return 0
-	else:
-		return(int((factorial(row)/(factorial(col)*(factorial(row-col))))))	
+    if(row == col and col == 1):
+        return 1
+    # as ncn-r and ncr will have same value and will be equal to n
+    elif(col == row-1 and col == 1):
+        return(row)
+    # ncr formula is not applicable when n<r
+    elif(col > row):
+        return 0
+    else:
+        return(int((factorial(row)/(factorial(col)*(factorial(row-col))))))
