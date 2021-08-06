e62a65df62b220b876f725d69a178470766e9ea0
sumakola <sumakola1106@msitprogram.net>
2021-08-06 11:22:49 +0530

    power sum soln

--- power sum/powersum.py
+++ power sum/powersum.py
@@ -1,33 +1,35 @@
 
 # powerSum(n, k) [15 pts]
-# Write the function powerSum(n, k) that takes two 
-# possibly-negative integers n and k and returns the 
-# so-called power sum: Specify the time complexity based 
-# on the solution that you have given. [Hint: This can be 
+# Write the function powerSum(n, k) that takes two
+# possibly-negative integers n and k and returns the
+# so-called power sum: Specify the time complexity based
+# on the solution that you have given. [Hint: This can be
 # solved in (n * log k)  or better.
 
 #    1**k + 2**k + ... + n**k
 
-# If n is negative, return 0. Similarly, if k is negative, 
+# If n is negative, return 0. Similarly, if k is negative,
 # return 0.
+
+# reference from geeksforgeeks
 
 def powersum(n, k):
     sum = 0
-    #according to the question
-    if(n<0 or k<0):
+    # according to the question
+    if(n < 0 or k < 0):
         return 0
-    #power sum, iterating thro all n nos, adding the powers of digits
+    # power sum, iterating thro all n nos, adding the powers of digits
     for i in range(1, n + 1):
         sum += power(i, k)
     return sum
 # Write your own test cases here...
 
-def power(a,b):
-    if a<0 or b<0:
+
+def power(a, b):
+    if a < 0 or b < 0:
         return 0
     else:
-        p=1
+        p = 1
         for i in range(b):
-            p=p*a
+            p = p*a
         return p
-
