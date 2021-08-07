75cec71f1c6ffd514216c053f6d9aaa26fccb8a4
sumakola <sumakola1106@msitprogram.net>
2021-08-07 22:42:08 +0530

    top scorer soln

--- topScorer.py
+++ topScorer.py
@@ -1,22 +1,43 @@
 # topScorer(data) [10 Points]
-# Write the function topScorer(data) that takes a multi-line 
-# string encoding scores as csv data for some kind of 
-# competition with players receiving scores, so each 
-# line has comma-separated values. The first value on 
-# each line is the name of the player (which you can 
-# assume has no integers in it), and each value after 
-# that is an individual score (which you can assume is a 
-# non-negative integer). You should add all the scores 
-# for that player, and then return the player with the 
-# highest total score. If there is a tie, return all the 
-# tied players in a comma-separated string with the names 
-# in the same order they appeared in the original data. 
-# If nobody wins (there is no data), return None (not the 
+# Write the function topScorer(data) that takes a multi-line
+# string encoding scores as csv data for some kind of
+# competition with players receiving scores, so each
+# line has comma-separated values. The first value on
+# each line is the name of the player (which you can
+# assume has no integers in it), and each value after
+# that is an individual score (which you can assume is a
+# non-negative integer). You should add all the scores
+# for that player, and then return the player with the
+# highest total score. If there is a tie, return all the
+# tied players in a comma-separated string with the names
+# in the same order they appeared in the original data.
+# If nobody wins (there is no data), return None (not the
 # string "None"). So, for example:
 
+# reference from
 def topScorer(data):
     # Your code goes here...
-    return ""
+    if(data == ""):
+        return None
+    a = []
+    b = []
+    spData = data.split("\n")
+    for i in range(len(spData) - 1):
+        a.append(spData[i].split(","))
+    for i in range(len(a)):
+        b.append(a[i][0])
+        del a[i][0]
+    x = max(a[0])
+    y = max(a[1])
+    if x > y:
+        return b[0]
+    elif y > x:
+        return b[1]
+    else:
+        seperator = ','
+        res = (str(seperator.join(b)))
+        return res
+
 
 data = '''\
 Fred,10,20,30,40
